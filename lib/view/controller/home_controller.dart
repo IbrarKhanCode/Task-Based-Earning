f46e079dd99a2398a78480d010d03402fb15d41c
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-09-24 19:53:44 +0500

    updated

--- lib/view/controller/home_controller.dart
+++ lib/view/controller/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -70,6 +71,51 @@ class HomeController extends GetxController{
   void tapRadio(int value){
 
     selectedRadio.value = value;
+
+  }
+
+  Future<void> updateProfile() async {
+
+    try{
+
+      User? user = auth.currentUser;
+      if(user != null){
+        await user.verifyBeforeUpdateEmail(emailController.text.trim());
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'email': emailController.text.trim(),
+          'firstName': firstNameController.text.trim(),
+          'lastName': lastNameController.text.trim(),
+          'createAt': DateTime.now(),
+        }, SetOptions(merge: true),
+        );}
+      Get.snackbar(
+        'Congratulation',
+        'You have updated the data',
+        colorText: Colors.white,
+        backgroundColor: Colors.green,
+        animationDuration: Duration(milliseconds: 300),
+        duration: Duration(seconds: 2),
+        borderRadius: 8,
+        borderColor: Colors.cyan,
+        borderWidth: 2,
+      );
+      Get.back();
+
+    }catch (e){
+
+      Get.snackbar(
+        'Error',
+        e.toString(),
+        colorText: Colors.white,
+        backgroundColor: Colors.red,
+        animationDuration: Duration(milliseconds: 300),
+        duration: Duration(seconds: 2),
+        borderRadius: 8,
+        borderColor: Colors.cyan,
+        borderWidth: 2,
+      );
+
+    }
 
   }
 
--- lib/view/profile/personal_info_screen.dart
+++ lib/view/profile/personal_info_screen.dart
@@ -24,196 +24,184 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(height: h * .05,),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: (){
-                      Get.back();
-                    },
-                    icon: Icon(Icons.arrow_back,),
-                  ),
-                  Text('Personal Information',style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600),),
-                ],
-              ),
-              SizedBox(height: 20,),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/Profile.png'),
-                radius: 40,
-              ),
-              SizedBox(height: 10,),
-              StreamBuilder<DocumentSnapshot>(
-                  stream: FirebaseFirestore.instance.collection('users').doc(user!.uid).snapshots(),
-                  builder: (context,snapshot){
-                    if(!snapshot.hasData){
-                      return Center(child: CircularProgressIndicator(),);
-                    }
-                    var userData = snapshot.data!.data() as Map<String, dynamic>;
-                    bool isGoogleLogin = !(userData.containsKey('firstName') && userData.containsKey('lastName'));
-                    return
-                      Column(
-                        children: [
-                          isGoogleLogin ?
-                          Text(userData['email'],style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.w500))
-                              : Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(userData['firstName'],style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 14),),
-                                  SizedBox(width: 5,),
-                                  Text(userData['lastName'],style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 14),),
-                                ],
-                              ),
-                              Text(userData['email'],style: TextStyle(color: Colors.grey,fontSize: 12,fontWeight: FontWeight.w500),),
-                            ],
-                          ),
-                        ],
-                      );
-                  }),
-              SizedBox(height: 10,),
-              Form(
-                key: _formKey,
-                child: Column(
+    return PopScope(
+      canPop: true,
+      onPopInvokedWithResult: (didPop, result){
+        if(didPop){
+          controller.firstNameController.clear();
+          controller.lastNameController.clear();
+          controller.emailController.clear();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(height: h * .05,),
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Text('First Name',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
-                      ],
-                    ),
-                    SizedBox(height: 10,),
-                    TextFormField(
-                      controller: controller.firstNameController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter Your First Name',
-                        prefixIcon: Icon(Icons.person_outline,color: Colors.grey,),
-                        hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                                color: Colors.grey
-                            )
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                              color: AppColors.primaryColor,
-                            )
-                        ),
-                      ),
-                      onChanged: controller.setFirstName,
-                      validator: controller.validateFirstName,
-                    ),
-                    SizedBox(height: 10,),
-                    Row(
-                      children: [
-                        Text('Last Name',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
-                      ],
-                    ),
-                    SizedBox(height: 10,),
-                    TextFormField(
-                      controller: controller.lastNameController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter Your Last Name',
-                        prefixIcon: Icon(Icons.person_outline,color: Colors.grey,),
-                        hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                                color: Colors.grey
-                            )
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                              color: AppColors.primaryColor,
-                            )
-                        ),
-                      ),
-                      onChanged: controller.setLastName,
-                      validator: controller.validateLastName,
-                    ),
-                    SizedBox(height: 10,),
-                    Row(
-                      children: [
-                        Text('Email',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
-                      ],
-                    ),
-                    SizedBox(height: 10,),
-                    TextFormField(
-                      controller: controller.emailController,
-                      decoration: InputDecoration(
-                        hintText: 'username555@gmail.com',
-                        prefixIcon: Icon(Icons.email_outlined,color: Colors.grey,),
-                        hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                                color: Colors.grey
-                            )
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(
-                              color: AppColors.primaryColor,
-                            )
-                        ),
-                      ),
-                      onChanged: controller.setEmail,
-                      validator: controller.validateEmail,
-                    ),
-                    SizedBox(height: h * .25),
-                    GestureDetector(
-                      onTap: (){
-                        if(_formKey.currentState!.validate()){
-                          Get.snackbar(
-                            'Congratulation',
-                            'Data Updated Successful',
-                            colorText: Colors.white,
-                            backgroundColor: Colors.green,
-                            animationDuration: Duration(milliseconds: 300),
-                            duration: Duration(seconds: 2),
-                            borderRadius: 8,
-                            borderColor: Colors.cyan,
-                            borderWidth: 2,
-                          );
-                        } else {
-                          Get.snackbar(
-                            'Validation Error',
-                            'Fix the Errors',
-                            colorText: Colors.white,
-                            backgroundColor: Colors.red,
-                            animationDuration: Duration(milliseconds: 300),
-                            duration: Duration(seconds: 2),
-                            borderRadius: 8,
-                            borderColor: Colors.cyan,
-                            borderWidth: 2,
-                          );
-                        }
+                    IconButton(
+                      onPressed: (){
+                        Get.back();
                       },
-                      child: Container(
-                        height: h * .06,
-                        width: w,
-                        decoration: BoxDecoration(
-                          color: AppColors.primaryColor,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Center(
-                          child: Text('SAVE',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 16),),
-                        ),
-                      ),
-                    )
+                      icon: Icon(Icons.arrow_back,),
+                    ),
+                    Text('Personal Information',style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600),),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(height: 20,),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/Profile.png'),
+                  radius: 40,
+                ),
+                SizedBox(height: 10,),
+                StreamBuilder<DocumentSnapshot>(
+                    stream: FirebaseFirestore.instance.collection('users').doc(user!.uid).snapshots(),
+                    builder: (context,snapshot){
+                      if(!snapshot.hasData){
+                        return Center(child: CircularProgressIndicator(),);
+                      }
+                      var userData = snapshot.data!.data() as Map<String, dynamic>;
+                      bool isGoogleLogin = !(userData.containsKey('firstName') && userData.containsKey('lastName'));
+                      return
+                        Column(
+                          children: [
+                            isGoogleLogin ?
+                            Text(userData['email'],style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.w500))
+                                : Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(userData['firstName'],style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 14),),
+                                    SizedBox(width: 5,),
+                                    Text(userData['lastName'],style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 14),),
+                                  ],
+                                ),
+                                Text(userData['email'],style: TextStyle(color: Colors.grey,fontSize: 12,fontWeight: FontWeight.w500),),
+                              ],
+                            ),
+                          ],
+                        );
+                    }),
+                SizedBox(height: 10,),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text('First Name',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        controller: controller.firstNameController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Your First Name',
+                          prefixIcon: Icon(Icons.person_outline,color: Colors.grey,),
+                          hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                  color: Colors.grey
+                              )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                color: AppColors.primaryColor,
+                              )
+                          ),
+                        ),
+                        onChanged: controller.setFirstName,
+                        validator: controller.validateFirstName,
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Text('Last Name',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        controller: controller.lastNameController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Your Last Name',
+                          prefixIcon: Icon(Icons.person_outline,color: Colors.grey,),
+                          hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                  color: Colors.grey
+                              )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                color: AppColors.primaryColor,
+                              )
+                          ),
+                        ),
+                        onChanged: controller.setLastName,
+                        validator: controller.validateLastName,
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Text('Email',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500),)
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      TextFormField(
+                        controller: controller.emailController,
+                        decoration: InputDecoration(
+                          hintText: 'username555@gmail.com',
+                          prefixIcon: Icon(Icons.email_outlined,color: Colors.grey,),
+                          hintStyle: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.w400),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                  color: Colors.grey
+                              )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(
+                                color: AppColors.primaryColor,
+                              )
+                          ),
+                        ),
+                        onChanged: controller.setEmail,
+                        validator: controller.validateEmail,
+                      ),
+                      SizedBox(height: h * .25),
+                      GestureDetector(
+                        onTap: (){
+                          if(_formKey.currentState!.validate()){
+                            controller.updateProfile();
+                          }
+                        },
+                        child: Container(
+                          height: h * .06,
+                          width: w,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Center(
+                            child: Text('SAVE',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 16),),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
